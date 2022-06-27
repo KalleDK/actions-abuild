8e77be90a36c5936f1e175bfd0765ff4d08b48a8
Kalle M Aagaard <git@k-moeller.dk>
2022-06-27 11:03:26 +0200

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ export SRCDEST="${GITHUB_WORKSPACE}/cache/distfiles"
 export PACKAGER="${INPUT_ABUILD_PACKAGER}"
 
 export PREFIX=${INPUT_ABUILD_PREFIX:-.}
-export COMMIT=${INPUT_ABUILD_COMMIT}
+export GIT_COMMIT=${INPUT_ABUILD_PKG_COMMIT}
 
 # Set version
 echo
