c9b9d2316b36312dce3091c924ff02d4ecea1db8
Kalle M Aagaard <git@k-moeller.dk>
2022-06-27 13:08:22 +0200

    key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 echo "::group::Setup"
 
 export REPODEST="${GITHUB_WORKSPACE}/packages"
@@ -10,11 +12,14 @@ export PREFIX=${INPUT_ABUILD_PREFIX:-.}
 export GIT_COMMIT=${INPUT_ABUILD_PKG_COMMIT}
 export PKG_VER=${INPUT_ABUILD_PKG_REL}
 export REPONAME=${INPUT_ABUILD_REPO_NAME}
+export ABUILD_DIR=~/.abuild
 
 # Set release
 if [[ ! -z "${INPUT_ABUILD_PKG_REL}" ]]; then
   export PKG_REL=${INPUT_ABUILD_PKG_REL}
 fi
+mkdir -p ${REPODEST}
+mkdir -p ${ABUILD_DIR}
 echo ""
 
 echo "::endgroup::"
@@ -23,17 +28,20 @@ echo "::group::Keys"
 # Set signing keys
 if [[ ! -z "${INPUT_ABUILD_KEY_NAME}" ]]; then
     echo "Using environment keys"
-    export PACKAGER_PRIVKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa"
+    export PACKAGER_PRIVKEY="${ABUILD_DIR}/${INPUT_ABUILD_KEY_NAME}.rsa"
     printf "${INPUT_ABUILD_KEY}" > "${PACKAGER_PRIVKEY}"
 
-    export PACKAGER_PUBKEY="/root/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
+    export PACKAGER_PUBKEY="${ABUILD_DIR}/${INPUT_ABUILD_KEY_NAME}.rsa.pub"
     printf "${INPUT_ABUILD_KEY_PUB}" > "${PACKAGER_PUBKEY}"
     
     cp "${PACKAGER_PUBKEY}" /etc/apk/keys/
 else
     echo "Using generated keys"
     abuild-keygen -a -i -n
+    export PACKAGER_PUBKEY="${ABUILD_DIR}/$(ls -1rt ${ABUILD_DIR} | grep \.rsa\.pub | tail -n 1 | tail -n 1)"
 fi
+cp ${PACKAGER_PUBKEY} ${REPODEST}/
+echo "::set-output name=packages_key::./packages/$(basename ${PACKAGER_PUBKEY})"
 
 echo "::endgroup::"
 echo "::group::Build"
